ecc315ca467b74f0eebc7aef81f9ad9ae7aca281
juan.brizuela <juan.brizuela@housecallpro.com>
2022-07-16 15:22:44 -0500

    Added pagy gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
+  include Pagy::Backend
+
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: %i[ new create update destroy ]
 
+  Pagy::DEFAULT[:items] = 2
+
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @pagy, @records = pagy(Post.all)
   end
 
   # GET /posts/1 or /posts/1.json
